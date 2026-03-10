b8b52b6ba4284dca3349814d539a6bbe134ab525
Ismaeel <ismaeel.moussa1@gmail.com>
2026-03-10 23:46:07 +0300

    Implement Repository Layer

---
+++ Assignments/Assignment_3/UniversitySystem/University.Data/Repository/StudentRepository.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using University.Data.AppDbContext;
+using University.Data.Entities;
+
+namespace University.Data.Repository
+{
+    public class StudentRepository(UniversityDbContext context) : IStudentRepository
+    {
+        private readonly UniversityDbContext _context = context;
+
+        public List<Student> GetAll()
+        {
+            return [.. _context.Students];
+        }
+
+        public Student GetById(int id)
+        {
+            return _context.Students.Find(id);
+        }
+
+        public void Add(Student student)
+        {
+            ArgumentNullException.ThrowIfNull(student);
+            _context.Students.Add(student);
+
+        }
+
+        public void Update(Student student)
+        {
+            ArgumentNullException.ThrowIfNull(student);
+            _context.Students.Update(student);
+        }
+
+        public void Detete(Student student)
+        {
+            ArgumentNullException.ThrowIfNull(student);
+            _context.Students.Remove(student);
+        }
+
+        public void SaveChanges()
+        {
+            _context.SaveChanges();
+        }
+
+    }
+
+    public interface IStudentRepository
+    {
+        List<Student> GetAll();
+        Student GetById(int id);
+        void Add(Student student);
+        void Update(Student student);
+        void Detete(Student student);
+        void SaveChanges();
+    }
+}
