dec73fd7e8cd48f55e2c3c551ae5d311082ac42a
Ismaeel <ismaeel.moussa1@gmail.com>
2026-03-17 22:46:46 +0300

    style: improve home page

--- Assignments/React-Assignment/src/pages/HomePage/HomePage.tsx
+++ Assignments/React-Assignment/src/pages/HomePage/HomePage.tsx
@@ -3,8 +3,10 @@ import './HomePage.scss';
 const HomePage = () => {
     return (
         <div className="home-page">
-            <h1>Welcome to our university system</h1>
-            <NavLink to="/students">Go to students</NavLink>
+            <h1>Welcome To Altinbas University System</h1>
+            <NavLink className="link" to="/students">
+                Go to students
+            </NavLink>
         </div>
     );
 };
